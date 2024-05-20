ccaa6ffd59cc0ba240e0930f4aa2fbc5a0b2b4e1
NeneNafe <nenechiwaya@gmail.com>
2024-05-20 22:15:19 +0200

    the HTTP status code for a request where the user is authenticate but not allowed to access to a resource? 403

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized():
     """unauthorised error """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """ the forbidden error """
+    abort(403)
