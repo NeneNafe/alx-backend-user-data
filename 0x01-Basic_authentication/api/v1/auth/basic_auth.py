266713fdccd293b59e2a83ae5994d5cdac9fdfd2
NeneNafe <nenechiwaya@gmail.com>
2024-05-21 11:05:24 +0200

    returns the Base64 part of the Authorization header for a Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,9 +4,21 @@
 from api.v1.auth.auth import Auth
 
 
+
 class BasicAuth(Auth):
     """_summary_
 
     Args:
         Auth (_type_): The class to inherit from
     """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str
+                                            ) -> str:
+        """ returns the Base64 part of the Authorization header
+        for a Basic Authentication """
+        if authorization_header is None or\
+            type(authorization_header) is not str:
+                return None
+        head = authorization_header.split(' ')
+
+        return head[1] if head[0] == 'Basic' else None
