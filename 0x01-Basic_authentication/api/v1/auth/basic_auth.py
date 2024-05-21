ffa667d44233f79eff73bc86502bb3b245c13e5e
NeneNafe <nenechiwaya@gmail.com>
2024-05-21 11:14:43 +0200

    Corrections

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,9 @@
 """ inherits from Auth """
 
 from api.v1.auth.auth import Auth
-
+import base64
+from flask import request
+from typing import List, TypeVar
 
 
 class BasicAuth(Auth):
@@ -17,8 +19,8 @@ class BasicAuth(Auth):
         """ returns the Base64 part of the Authorization header
         for a Basic Authentication """
         if authorization_header is None or\
-            type(authorization_header) is not str:
-                return None
+           type(authorization_header) is not str:
+            return None
         head = authorization_header.split(' ')
 
         return head[1] if head[0] == 'Basic' else None
