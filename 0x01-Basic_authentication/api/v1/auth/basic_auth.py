a9bdfa88ac381d25e98f8d6fd826f9850966e971
NeneNafe <nenechiwaya@gmail.com>
2024-05-21 10:11:30 +0200

    a class BasicAuth that inherits from Auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" inherits from Auth """
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """_summary_
+
+    Args:
+        Auth (_type_): The class to inherit from
+    """
