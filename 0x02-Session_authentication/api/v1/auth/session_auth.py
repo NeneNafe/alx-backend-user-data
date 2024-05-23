fa39db57506809ed8221d67d1a5a41fefe35cb29
NeneNafe <nenechiwaya@gmail.com>
2024-05-23 15:33:46 +0200

    a class attribute user_id_by_session_id initialized by an empty dictionary

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,7 +2,18 @@
 """ a class SessionAuth that inherits from Auth """
 
 from api.v1.auth.auth import Auth
+import uuid
+from models.user import User
 
 
-class SessionAuth(Auth):
+class SessionAuth (Auth):
     """ Inherits from Auth """
+    user_id_by_session_id = {}
+    
+    def create_session(self, user_id: str = None) -> str:
+        """" creates a Session ID for a user_id """
+        if user_id is None or not str:
+            return None
+        session_id = str(uuid.uuid4)
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
