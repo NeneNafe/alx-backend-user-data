2cb5e197c394eed90403480e9281cf3cfe197766
NeneNafe <nenechiwaya@gmail.com>
2024-05-23 21:59:39 +0200

    Corrections

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -48,18 +48,13 @@ def before_request():
     """
     paths = ['/api/v1/status/', '/api/v1/unauthorized/',
              '/api/v1/forbidden/', '/api/v1/auth_session/login/']
-    if not auth:
-        return None
-    if not auth.require_auth(request.path, paths):
-        return None
-    if not auth.authorization_header(request):
-        abort(401)
-    if not auth.current_user(request):
-        abort(403)
-    request.current_user = auth.current_user(request)
-    if auth.authorization_header(request) and\
-        auth.session_cookie(request):
-            return None, abort(401)
+    if auth and auth.require_auth(request.path, paths):
+        if (not auth.authorization_header(request) and
+           not auth.session_cookie(request)):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
+        request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
