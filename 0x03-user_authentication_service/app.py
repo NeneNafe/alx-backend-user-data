830c00bcb34447fbfbd63b3f103660fec1881a02
NeneNafe <nenechiwaya@gmail.com>
2024-05-28 15:37:17 +0200

    Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index() -> str:
     """ The flask function """
-    return josnify({"message": "Bienvenue"})
+    return jsonify({"message": "Bienvenue"})
 
 
 if __name__ == "__main__":
