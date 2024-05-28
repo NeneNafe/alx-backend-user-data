14e31f4f56c2e8ce20423dccc506b50850a78ca0
NeneNafe <nenechiwaya@gmail.com>
2024-05-28 20:45:51 +0200

    Get session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -46,3 +46,13 @@ class Auth:
                                   user.hashed_password)
         except (NoResultFound, IndentationError):
             return False
+
+    def create_session(self, email: str) -> str:
+        """ Creates a new session id """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
