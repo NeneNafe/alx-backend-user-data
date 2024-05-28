f708d342a208a121b8cd49831412584912fbfa76
NeneNafe <nenechiwaya@gmail.com>
2024-05-28 20:24:55 +0200

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@
 import bcrypt
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -30,3 +31,12 @@ class Auth:
             return self._db.add_user(email, _hash_password(password))
         else:
             raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Ensures that user login is valid """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except (NoResultFound, IndentationError):
+            return False
