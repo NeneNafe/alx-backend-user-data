2ce24c8660da2d508bbaeac32d26aec357ef6bcd
NeneNafe <nenechiwaya@gmail.com>
2024-05-28 09:22:56 +0200

    Corrections

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,7 +4,9 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext import InvalidRequestError
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 from typing import TypeVar
 
